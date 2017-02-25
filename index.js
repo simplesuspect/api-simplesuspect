00725843829dcf9acc6458270b35afef10857002
marcskaarup <marcskaarup@gmail.com>
2017-02-25 12:42:10 -0800

    Clean up code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const server = require('http').createServer(app);
 const path = require('path');
+const util = require('./utility.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -11,7 +12,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.post('/s', function (req, res) {
-	var image = decodeBase64Image(req.body.data);
+	var image = util.decodeBase64Image(req.body.data);
 	res.status(200).send(image);
 });
 
@@ -19,16 +20,3 @@ server.listen(3001, function () {
 	console.log('Server listening at port %d', 3001);
 });
 
-function decodeBase64Image(dataString) {
-  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-     response = {};
-
-  if (matches.length !== 3) {
-    return new Error('Invalid input string');
-  }
-
-  response.type = matches[1];
-  response.data = new Buffer(matches[2], 'base64');
-
-  return response;
-}
