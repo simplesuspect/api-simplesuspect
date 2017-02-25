5dd6f431e02eeee0372f1d8863eacf559ddc4144
Marc Skaarup <Marc Skaarup>
2017-02-25 12:11:06 -0800

    POST

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ const app = express();
 const bodyParser = require('body-parser');
 const server = require('http').createServer(app);
 const path = require('path');
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-
-
-app.get('/suspect', function (req, res) {
-	res.status(200).send('you are guilty');
+app.post('/s', function (req, res) {
+	var image = decodeBase64Image(req.body.data);
+	res.status(200).send(image);
 });
 
-
-server.listen(3000, function () {
-	console.log('Server listening at port %d', 3000);
+server.listen(3001, function () {
+	console.log('Server listening at port %d', 3001);
 });
+
+function decodeBase64Image(dataString) {
+  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+     response = {};
+
+  if (matches.length !== 3) {
+    return new Error('Invalid input string');
+  }
+
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], 'base64');
+
+  return response;
+}
