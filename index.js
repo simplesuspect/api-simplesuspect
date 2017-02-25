8fc1533b25a260a3de2780f990cc3b9c67e61123
marcskaarup <marcskaarup@gmail.com>
2017-02-25 15:12:06 -0800

    Working Face API

--- index.js
+++ index.js
@@ -4,19 +4,34 @@ const app = express();
 const bodyParser = require('body-parser');
 const server = require('http').createServer(app);
 const path = require('path');
-const util = require('./utility.js');
+
+const key = "5a7bc1aa7775473dbc2a587a623682d5";
+const oxford = require('project-oxford');
+const client = new oxford.Client(key, 'https://westus.api.cognitive.microsoft.com');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+
 app.post('/s', function (req, res) {
-	var image = util.decodeBase64Image(req.body.data);
-	res.status(200).send(image);
+	client.face.detect({
+		data: oxford.makeBuffer(req.body.data),
+		returnFaceId: true,
+		analyzesAge: true,
+		analyzesGender: true,
+		analyzesFaceLandmarks: true,
+		analyzesSmile: true,
+		analyzesFacialHair: true,
+		analyzesHeadPose: true
+	}).then(function (response) {
+		console.log(response);
+		
+		res.status(200).send(response);
+	});
 });
 
 server.listen(3001, function () {
 	console.log('Server listening at port %d', 3001);
 });
-
