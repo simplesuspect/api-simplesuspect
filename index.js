dd6f6004a38da03d796d6116f7d25161e9afaac1
Nicholas Kristoffersen <nicholas.kristoffersen@gmail.com>
2017-02-25 18:46:34 -0800

    increase payload

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
-
+app.use(bodyParser.json({
+	limit: '5mb'
+}));
+app.use(bodyParser.urlencoded({
+	limit: '5mb'
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
